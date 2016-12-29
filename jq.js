71b7004bd3aede5a3049cb9f9fbfc1c3712df703
sam4spires <samthg@gmail.com>
2016-12-29 19:50:00 +0000

    Add files via upload

--- jq.js
+++ jq.js
@@ -5,13 +5,13 @@ $(document).ready(function(){
 	function animation1()
 	{
 		if (m==''){
-		$(this).animate({left:"550px",opacity:"0.5"}, 1000) ;
-        $('#availability').animate({left:'550px'},1000).slideToggle(1000); m = 'moved';}
+		$('#wrapped').children().animate({left:"690px",opacity:"0.9"}, 1000) ;
+        $('#availability').toggle(1); m = 'moved';}
         
         else if(m=='moved'){
-        $(this).animate({left:"1000px",opacity:"1"},1000) ;
-        $('#availability').animate({left:'1000px'},1000).slideToggle(10); m = '';}
-
+        $('#availability').toggle(1); m = '';
+        $('#wrapped').children().animate({left:"1000px",opacity:"1"},1000);}
+        
 	}
 
 	function animation2()
@@ -24,7 +24,7 @@ $(document).ready(function(){
     	else if(m2=='moved')
     	{
     		$(this).animate({fontSize:"1em"},1000); m2='';
-	    	$(this).animate({left:"150px",top:"300px",opacity:"1", width: "60",height:"20px"}, 1000 );
+	    	$(this).animate({left:"160px",top:"300px",opacity:"1", width: "60",height:"20px"}, 1000 );
 	    	$(".review").toggle()
     	
 	    	
@@ -32,8 +32,13 @@ $(document).ready(function(){
 
     	}
 
+    
+     		
 	$("#dropav").on('click', animation1);
+	$(".day").on('click', function(){ $(this).animate({right:'100px'})})
     $("#revclick").on('click',animation2);
+    $("#prices").on('click', function(){
+    	$("#tabprices").toggle()})
 
     $(".day").on('click',function(){    	
   		var t = $(this).attr('data-did');
@@ -45,6 +50,17 @@ $(document).ready(function(){
         $("#"+d).slideToggle("slow");
       });
     
+   $("#subjects").children().on('click', function() {
 
-    });
+   		subj = $(this).attr('id')
+   		if (subj == 'sub1')
+   			{$('iframe').attr('src','https://www.youtube.com/embed/Gcby6WYAVKg/')}
+   		else if (subj == "sub2")
+   			{$('iframe').attr('src','https://www.youtube.com/embed/2vsAY-tp9js?list=PL5D99A319E129A5B7/')}
+   		
+   	});
+
+
+
+})
 
